2f69754ccfdc68bf80d31f7cfd1a0f5423471ec1
DanielGG1024 <a22472510@gmail.com>
2021-07-05 20:46:52 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser')
 const app = express()
+
+mongoose.connect('mongodb://localhost/restaurants', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
 const restaurants = require('./restaurant.json')
 const allrestaurant = restaurants.results
 const port = 3000
 
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
+
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
 app.use(express.static('public'))
+// app.use(bodyParser.urlencoded({ extended: true }))
+
 // 主頁面
 app.get('/', (req, res) => {
-    res.render('index', { restaurants: allrestaurant })
+    Restaurant.find()
+        .lean()
+        .then(restaurants => res.render('index', { restaurants }))
+        .catch(error => console.error(error))
 })
 // 搜尋
 app.get('/search', (req, res) => {
     const searchWord = req.query.keyword.toLowerCase().trim()
     const searchRestaurant = allrestaurant.filter(restaurant =>
-        restaurant.name.toLocaleLowerCase().trim().includes(searchWord)||
+        restaurant.name.toLocaleLowerCase().trim().includes(searchWord) ||
         restaurant.category.trim().includes(searchWord))
     res.render('index', { restaurants: searchRestaurant })
 })
 
 app.get('/restaurants/:keyword', (req, res) => {
     const paramsId = req.params.keyword
-    const clickRestaurant = allrestaurant.find(restaurant => restaurant.id.toString() === paramsId)
-    res.render('show', { restaurant: clickRestaurant })
+    Restaurant.findById(paramsId)
+              .lean()
+              .then(restaurant => res.render('show',{restaurant}))
+              .catch(error =>console.log('error'))
+
+    // const clickRestaurant = allrestaurant.find(restaurant => restaurant.id.toString() === paramsId)
+    // res.render('show', { restaurant: clickRestaurant })
 })
 
 app.listen(port, () => {
---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant')
+const restaurantsJson = require('../../restaurant.json')
+const allrestaurant = restaurantsJson.results
+mongoose.connect('mongodb://localhost/restaurants', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+
+db.once('open', () => {
+    console.log('mongodb connceted!')
+    allrestaurant.forEach((item)=>{
+        Restaurant.create({
+            name: item.name,
+            name_en: item.name_en,
+            category:item.category,
+            image:item.image,
+            location:item.location,
+            phone:item.phone,
+            google_map:item.google_map,
+            rating:item.rating,
+            description:item.description
+        })
+    })
+    console.log('done')
+})
