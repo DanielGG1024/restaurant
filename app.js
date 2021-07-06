53acc3d28da00326fadb410e92492edc7c0cba5c
DanielGG1024 <a22472510@gmail.com>
2021-07-06 15:30:25 +0800

    feat: add restaurant edit and delete function

--- app.js
+++ app.js
@@ -20,11 +20,12 @@ db.once('open', () => {
 })
 
 const exphbs = require('express-handlebars')
+const restaurant = require('./models/restaurant')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
-// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // 主頁面
 app.get('/', (req, res) => {
@@ -41,16 +42,56 @@ app.get('/search', (req, res) => {
         restaurant.category.trim().includes(searchWord))
     res.render('index', { restaurants: searchRestaurant })
 })
-
-app.get('/restaurants/:keyword', (req, res) => {
-    const paramsId = req.params.keyword
+//瀏覽單一餐廳detail
+app.get('/restaurants/:id', (req, res) => {
+    const paramsId = req.params.id
     Restaurant.findById(paramsId)
-              .lean()
-              .then(restaurant => res.render('show',{restaurant}))
-              .catch(error =>console.log('error'))
-
-    // const clickRestaurant = allrestaurant.find(restaurant => restaurant.id.toString() === paramsId)
-    // res.render('show', { restaurant: clickRestaurant })
+        .lean()
+        .then(restaurant => res.render('detail', { restaurant }))
+        .catch(error => console.log(error))
+})
+// 導向編輯頁面
+app.get('/restaurants/:id/edit', (req, res) => {
+    const paramsId = req.params.id
+    Restaurant.findById(paramsId)
+        .lean()
+        .then((restaurant) => res.render('edit', { restaurant }))
+        .catch(error => console.log(error))
+})
+// 修改資料
+app.post('/restaurants/:id/edit', (req, res) => {
+    const id = req.params.id
+    const name = req.body.name
+    const name_en = req.body.name_en
+    const category = req.body.category
+    const image = req.body.image
+    const location = req.body.location
+    const phone = req.body.phone
+    const google_map = req.body.google_map
+    const rating = req.body.rating
+    const description = req.body.description
+    return Restaurant.findById(id)
+        .then(restaurant => {
+            restaurant.name = name
+            restaurant.name_en = name_en
+            restaurant.category = category
+            restaurant.image = image
+            restaurant.location = location
+            restaurant.phone = phone
+            restaurant.google_map = google_map
+            restaurant.rating = rating
+            restaurant.description = description
+            return restaurant.save()
+        })
+        .then(() => res.redirect(`/restaurants/${id}`))
+        .catch(error => console.log(error))
+})
+app.post('/restaurants/:id/delete', (req, res) => {
+    const id = req.params.id
+    return Restaurant.findById(id)
+        .then(Restaurant => Restaurant.remove())
+        .then(()=>res.redirect('/'))
+        .catch(error=>console.log(error))
 })
 
 app.listen(port, () => {
